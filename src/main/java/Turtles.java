1df5acd0c240ce2b758ba677a32edaaa36066cb1
Jared <jared@Zipcoders-MacBook-Pro-4.local>
2024-07-27 20:22:01 -0400

    done

--- src/main/java/Turtles.java
+++ src/main/java/Turtles.java
@@ -1,7 +1,51 @@
-package PACKAGE_NAME;
+import java.time.Duration;
+import java.time.Instant;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class Turtles {
+
+    int factorial(int n) {
+        if (n <= 1) return 1;
+        return n * factorial(n - 1);
+    }
+
+    // recursive approach
+    public int gcd(int p, int q) {
+        if (q == 0) return p;
+        else return gcd(q, p % q);
+    }
+
+    //non-recursive approach
+    public static int gcd2(int p, int q) {
+        while (q != 0) {
+            int temp = q;
+            q = p % q;
+            p = temp;
+        }
+        return p;
+    }
+
+    public static void main(String[] args) {
+        Turtles turtles = new Turtles();
+
+        long timeElapsed = 0;
+        for (int i = 0; i < 100; i++) {
+            Instant start = Instant.now();
+            turtles.gcd(123456789, 987654321);
+            Instant finish = Instant.now();
+            timeElapsed += Duration.between(start, finish).toNanos();
+        }
+        System.out.println(timeElapsed);
+
+        long timeElapsed2 = 0;
+        for (int i = 0; i < 100; i++) {
+            Instant start2 = Instant.now();
+            turtles.gcd2(123456789, 987654321);
+            Instant finish2 = Instant.now();
+            timeElapsed2 += Duration.between(start2, finish2).toNanos();
+        }
+        System.out.println(timeElapsed2);
+    }
 }
--- src/test/java/TurtlesTest.java
+++ src/test/java/TurtlesTest.java
@@ -1,8 +1,40 @@
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by kristofer on 7/14/20.
  */
 public class TurtlesTest {
+
+    @Test
+    public void factorialTest1(){
+        Turtles turtles = new Turtles();
+        Assert.assertEquals(120,turtles.factorial(5));
+    }
+
+    @Test
+    public void factorialTest2(){
+        Turtles turtles = new Turtles();
+        Assert.assertEquals(3628800, turtles.factorial(10));
+    }
+
+    @Test
+    public void factorialTest3(){
+        Turtles turtles = new Turtles();
+        Assert.assertEquals(5040, turtles.factorial(7));
+    }
+
+    @Test
+    public void gcdTest1(){
+        Turtles turtles = new Turtles();
+        Assert.assertEquals(24, turtles.gcd(1440, 408));
+    }
+
+    @Test
+    public void gcdTest2(){
+        Turtles turtles = new Turtles();
+        Assert.assertEquals(24, turtles.gcd(408, 216));
+    }
+
 
 }
